46e667aaf958ec2cd5e60b40b501e581c2614ab0
tiationg kho <tiationgkho@gmail.com>
2022-06-22 01:58:10 -0700

    fix

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,9 +1,15 @@
 import Head from 'next/head'
+import React, { useEffect, useState } from 'react'
 import { PostCard, Categories, PostWidget } from '../components/index'
 import { getPosts } from '../services/index'
-import {FeaturedPosts} from '../sections'
+import { FeaturedPosts } from '../sections'
 
 const Home = ({ posts }) => {
+	const [curPosts, setCurPosts] = useState([])
+	useEffect(() => {
+		setCurPosts(posts)
+	}, [posts])
+
 	return (
 		<>
 			<div className='container mx-auto px-10 mb-8'>
@@ -14,7 +20,7 @@ const Home = ({ posts }) => {
 				<FeaturedPosts />
 				<div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
 					<div className='lg:col-span-8 col-span-1'>
-						{posts.map((post, index) => (
+						{curPosts.map((post, index) => (
 							<PostCard key={post.node.title} post={post.node} />
 						))}
 					</div>
